464b648a94b3e977bf5e47a3aa08800765039947
Steven Nagie <sanagie@yahoo.com>
2016-10-24 17:06:27 -0600

    Got local storage working with my user profiles.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,8 +2,14 @@ angular.module('devMtnSocial')
 .controller('mainCtrl', function($scope){
   $scope.users = [];
 
+  if (localStorage.list) {
+    $scope.users = JSON.parse(localStorage.getItem('list'));
+    console.log($scope.users);
+  }
+
   $scope.addNewUser = function(newName, newTag, newImg, newBio) {
     $scope.users.push({name: newName, tag: newTag, img: newImg, bio: newBio});
+    localStorage.setItem('list', JSON.stringify($scope.users));
     console.log($scope.users);
   };
 

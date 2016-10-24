d61a9ab8b71c884c8ad5c2da962a2d8eaaee1560
Steven Nagie <sanagie@yahoo.com>
2016-10-24 17:12:07 -0600

    Added comments and notes to round out the day.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,13 +2,16 @@ angular.module('devMtnSocial')
 .controller('mainCtrl', function($scope){
   $scope.users = [];
 
+  // All the stuff contained here is for saving user profiles to the local storage.
   if (localStorage.list) {
     $scope.users = JSON.parse(localStorage.getItem('list'));
     console.log($scope.users);
   }
+  // *************************************
 
   $scope.addNewUser = function(newName, newTag, newImg, newBio) {
     $scope.users.push({name: newName, tag: newTag, img: newImg, bio: newBio});
+    // This line immediately pushes any new user profiles to the local storage
     localStorage.setItem('list', JSON.stringify($scope.users));
     console.log($scope.users);
   };

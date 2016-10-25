3df0f496908f5965629ad1b15e27752d2aa2a6eb
Steven Nagie <sanagie@yahoo.com>
2016-10-25 16:09:50 -0600

    Improved styling, fixed triangles to have background-images, set up angular push of user input link for image.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -16,7 +16,11 @@ angular.module('devMtnSocial')
     console.log($scope.users);
   };
 
+  // These variables are all used as true/false toggles for adding classes to divs during the change from entry page to landing page.
   $scope.signUpHidden = false;
   $scope.userProfileHidden = true;
+  $scope.trianglesHidden = false
 
+  // The following creates a variable containing the image of the latest user, which we then push into the bottom triangle using ng-style.
+  $scope.userImage = "url(" + $scope.users[$scope.users.length - 1].img + ")";
 });

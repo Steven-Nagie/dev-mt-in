bf2abe31a4808f4f2c4a62f2445f75e4311b0117
Steven Nagie <sanagie@yahoo.com>
2016-10-25 16:49:42 -0600

    Got update profile functional, still needs to be styled.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,8 @@
 angular.module('devMtnSocial')
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl', function($scope, updateProfile){
   $scope.users = [];
+
+
 
   // All the stuff contained here is for saving user profiles to the local storage.
   if (localStorage.list) {
@@ -16,10 +18,28 @@ angular.module('devMtnSocial')
     console.log($scope.users);
   };
 
+  $scope.changeUser = function(newName, newTag, newImg, newBio) {
+    var userList = $scope.users;
+    if (newName) {
+      userList[userList.length - 1].name = newName;
+    } else if (newTag) {
+      userList[userList.length - 1].tag = newTag;
+    } else if (newImg) {
+      userList[userList.length - 1].img = newImg;
+    } else if (newBio) {
+      userList[userList.length - 1].bio = newBio;
+    }
+    $scope.users = userList;
+    localStorage.setItem('list', JSON.stringify($scope.users));
+    $scope.userImage = "url(" + $scope.users[$scope.users.length - 1].img + ")";
+    console.log($scope.users);
+  };
+
   // These variables are all used as true/false toggles for adding classes to divs during the change from entry page to landing page.
   $scope.signUpHidden = false;
   $scope.userProfileHidden = true;
-  $scope.trianglesHidden = false
+  $scope.trianglesHidden = false;
+  $scope.updateHidden = true;
 
   // The following creates a variable containing the image of the latest user, which we then push into the bottom triangle using ng-style.
   $scope.userImage = "url(" + $scope.users[$scope.users.length - 1].img + ")";
---
+++ js/upProfileService.js
@@ -0,0 +1,5 @@
+angular.module('devMtnSocial').service('updateProfile', function() {
+
+  
+
+});

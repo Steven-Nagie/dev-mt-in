d1c809da146aa2ffcbb52e6d670ec59a68a313df
Steven Nagie <sanagie@yahoo.com>
2016-10-28 13:17:42 -0600

    Got the hover effects on the thumbnails, and the blue overlay on the main photo picture.

--- js/friendStorage.js
+++ js/friendStorage.js
@@ -6,7 +6,7 @@ angular.module('devMtnSocial').service('friendService', function() {
       this.friends.push({name: friendName, tag: friendTag, bio: friendBio, img: friendImg});
   };
 
-  this.createFriends('Sally Rally', 'My name is Sally.', 'I left my heart in San Francsico.', 'https://images.unsplash.com/photo-1473163928189-364b2c4e1135?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=0721657b46ce27df52955f73818ab5b7');
+  this.createFriends('Sally Rally', 'My name is Sally.', 'I left my heart in San Francsico.', 'https://images.unsplash.com/photo-1473163928189-364b2c4e1135?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=0721657b46ce27df52955f73818ab5b7.jpg');
 
   this.createFriends('Gregory Isaacs', 'If I don\'t if I don\'t', "I don't know what we're yelling about", 'http://cdn3.pitchfork.com/artists/8438/m.f18e6321.jpg');
 
@@ -16,6 +16,6 @@ angular.module('devMtnSocial').service('friendService', function() {
 
   this.createFriends("Meshuggah", "Bleed", "Beams of fire sweep through my head\nThrusts of pain increasingly engaged\nSensory receptors succumb\nI'm no one now, only agony\nMy crimson liquid so frantically spilled\nThe ruby fluid of\nlife unleashed\nRipples ascend to the surface of my eyes\nTheir red pens\ndrawing at random, at will\nA myriad pains begotten in their wake\nThe bastard spawn of a mutinous self", "http://www.metalinjection.net/wp-content/uploads/2015/03/meshuggah28082013.jpg");
 
-  this.createFriends("Blackbeard", "Y'ARRR!", "YYY'AAAAAARRRRRRRRR", "https://upload.wikimedia.org/wikipedia/commons/f/f7/General_History_of_the_Pyrates_-_Blackbeard_the_Pirate_(1725).jpg");
+  this.createFriends("Blackbeard", "Y'ARRR!", "YYY'AAAAAARRRRRRRRR", "http://mrnussbaum.com/images/blackbeard.jpg");
 
 });

a6cadbf513dc54a9006bc63e20a1226d290a9030
Steven Nagie <sanagie@yahoo.com>
2016-11-01 14:21:13 -0600

    Added an easter egg and formatted profile main name to be offset as in the design docs.

--- js/animations.js
+++ js/animations.js
@@ -8,4 +8,28 @@ $(document).ready(function() {
     $('#slide-menu-button').slideToggle();
     $('#search-for-friends').slideToggle();
   });
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters.charAt(Math.floor(Math.random() * 16));
+    }
+    return color;
+  }
+
+  $('p').click(function() {
+    var words = $(this).text().split(' ');
+    console.log('thieks');
+
+    for (var i = Math.floor(Math.random() * (5 - 1)); i < words.length; i) {
+      var random = getRandomColor();
+      words[i] = "<span style='color:" + random + "'>" + words[i] +"</span>";
+      i += Math.floor(Math.random() * (5 - 1));
+    }
+    words = words.join(" ");
+    $(this).html(words);
+  });
+
+
 });
